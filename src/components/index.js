fcc71a0c8588b7c13cbc515dca73aaf9b43a6399
hov92 <dhov92@outlook.com>
2025-10-05 00:33:59 -0400

    UPDATE

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
 import Hero from './Hero';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Works from './Works';
@@ -8,7 +8,7 @@ import Contact from './Contact';
 
 export {
   Hero,
-  Navbar,
+  // Navbar,
   About,
   Tech,
   Works,
